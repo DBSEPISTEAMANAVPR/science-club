5337f212b8854361ae6d5d35a6fa2dc719867f96
DBSEPISTEAMANAVPR <151384113+DBSEPISTEAMANAVPR@users.noreply.github.com>
2023-11-19 06:56:40 +0530

    added .css

---
+++ script.js
@@ -0,0 +1,8 @@
+var menuIcon  = document.querySelector(".menu-icon");
+var menuIcon  = document.querySelector(".sidebar");
+
+
+menuIcon.onclick = function(){
+    slidebar.classList.toggle("small-slidebar");
+}
+
